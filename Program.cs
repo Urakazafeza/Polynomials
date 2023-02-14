2bc1d1d23896e4fa27992486200f6c29b4c97361
uraka <uraka@URAKAZA15>
2023-02-14 02:08:22 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using Polynomials;
+using System;
+using System.Collections.Generic;
+
+namespace Polynomial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args is null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+            Console.WriteLine("Please input points in x y representation.\nType END to finish.");
+            int index = 1;
+            try
+            {
+                VandermondeMatrixClass matrix = new VandermondeMatrixClass();
+                while (true)
+                {
+                    Console.Write($"P#{index++}: ");
+                    String input = Console.ReadLine();
+                    if (input.Trim().ToUpper() == "END" || input.Trim() == "")
+                        break;
+                    matrix.AddEquation(new PointClass(input));
+                }
+                matrix.toVandermonde();
+                Console.WriteLine("Resulting polynomial will be of the order - " + Polynomial_class.CalculateOrder(matrix) + "\nCalculated polynomial:");
+                double[] coefficients = matrix.GetResults();
+                Console.WriteLine(Polynomial_class.Format(coefficients));
+                Dictionary<int, double> calculatedPolynomial = Polynomial_class.Calculate(coefficients);
+                foreach (KeyValuePair<int, double> value in calculatedPolynomial)
+                    Console.WriteLine($"f({value.Key}) = {value.Value:0.000}");
+                Console.WriteLine("Derivative:\n" + Polynomial_class.CalculateDerivative(coefficients));
+                double inititalGuess = 2;
+                Console.WriteLine("Looking for a root with initial guess 2");
+                Console.WriteLine("Root found for x = " + Polynomial_class.CalculateRoot(coefficients, inititalGuess).ToString("0.00000"));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error occured: " + e.Message);
+            }
+        }
+    }
+}
